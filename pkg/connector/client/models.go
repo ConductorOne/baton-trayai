74ae51a433fa202f60e57f39f066cf9dbd972df1
subencheng <ben.su@conductorone.com>
2025-04-09 14:57:21 -0700

    [BB-451] baton-trayai: remove custom user type

--- pkg/connector/client/models.go
+++ pkg/connector/client/models.go
@@ -1,23 +1,12 @@
 package client
 
-// TrayType is the Tray.ai's type.
-type TrayType int
-
-const (
-	Embeded TrayType = iota + 1
-	Organiaztion
-	Personal
-	PersonalExternal
-	Shared
-)
-
 // User is the Tray.ai user.
 type User struct {
-	ID               string   `json:"id"`
-	Name             string   `json:"name"`
-	Type             TrayType `json:"type"`
-	Description      string   `json:"description"`
-	MonthlyTaskLimit int64    `json:"monthlyTaskLimit"`
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	Type             string `json:"type"`
+	Description      string `json:"description"`
+	MonthlyTaskLimit int64  `json:"monthlyTaskLimit"`
 }
 
 type PageInfo struct {
