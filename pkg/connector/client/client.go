66dfcd2add9980de6eb1216d394d337dad8960b0
Bencheng21 <ben.su@conductorone.com>
2025-04-21 11:34:48 -0700

    [BB-451] baton-trayai: accept json body when creating accounts (#10)
    * [BB-451] baton-trayai: accept json body when creating accounts
    
    * delete to Values

--- pkg/connector/client/client.go
+++ pkg/connector/client/client.go
@@ -81,9 +81,9 @@ func (c *Client) ListUsers(ctx context.Context, params ListParams) (*ListResp, e
 
 // CreateUserParams is the parameters passed to CreateUser().
 type CreateUserParams struct {
-	Name               string
-	Email              string
-	OrganizationRoleId string
+	Name               string `json:"name"`
+	Email              string `json:"email"`
+	OrganizationRoleId string `json:"organizationRoleId"`
 }
 
 // CreateUser is used to create user in tray.ai.
@@ -96,9 +96,8 @@ func (c *Client) CreateUser(ctx context.Context, params *CreateUserParams) (*Use
 	req, err := c.httpClient.NewRequest(ctx,
 		http.MethodPost,
 		urlpath,
-		uhttp.WithFormBody(toValues(params)),
+		uhttp.WithJSONBody(params),
 		uhttp.WithAcceptJSONHeader(),
-		uhttp.WithContentTypeJSONHeader(),
 	)
 	if err != nil {
 		return nil, err
@@ -301,20 +300,4 @@ func (c *Client) GetWorkspace(ctx context.Context, workspaceID string) (*Workspa
 
 	defer rawResp.Body.Close()
 	return resp, nil
-}
-
-func toValues(p *CreateUserParams) string {
-	values := url.Values{}
-	if p.Name != "" {
-		values.Add("name", p.Name)
-	}
-
-	if p.Email != "" {
-		values.Add("email", p.Email)
-	}
-
-	if p.OrganizationRoleId != "" {
-		values.Add("organizationRoleId", p.OrganizationRoleId)
-	}
-	return values.Encode()
 }
